0ad08a0edbccbe8279dde4a0104d6eb4c7ec1181
Miles <mae9775@nyu.edu>
2021-11-11 10:47:40 -0500

    added pages

--- Assets/MilesSceneStuff/JournalManager.cs
+++ Assets/MilesSceneStuff/JournalManager.cs
@@ -1,11 +1,37 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Collections.Generic;
+
 
 public class JournalManager : MonoBehaviour
 {
     public Animator myAnim;
- 
+    public List<Sprite> pageSprite;
+
+    public GameObject pg1;
+    public GameObject pg2;
+
+    public int leftPageIndex = 0;
+    public int rightPageIndex = 1;
+   
+    public GameObject newPageLeft;
+    public GameObject newPageRight;
+
+    public int pagesMax = 10;
+
+
+    private void Start()
+    {
+        newPageLeft.transform.position = new Vector3(pg1.transform.position.x, pg1.transform.position.y);
+        newPageLeft.transform.SetParent(pg1.transform);
+        newPageLeft.GetComponent<SpriteRenderer>().sprite = pageSprite[leftPageIndex];
+
+        newPageRight.transform.position = new Vector3(pg2.transform.position.x, pg2.transform.position.y);
+        newPageRight.transform.SetParent(pg2.transform);
+        newPageRight.GetComponent<SpriteRenderer>().sprite = pageSprite[rightPageIndex];
+    }
+
 
     private void Update()
     {
@@ -36,15 +62,37 @@ public class JournalManager : MonoBehaviour
             if (Input.GetKey(KeyCode.A))
             {
                 Debug.Log("back");
+                if (Input.GetKeyDown(KeyCode.A) && rightPageIndex > 1.9)
+                {
+                    rightPageIndex -= 2;
+                    leftPageIndex -= 2;
+                    pageInit(newPageLeft, pg1, leftPageIndex);
+                    pageInit(newPageRight, pg2, rightPageIndex);
+                }
                 //display previous 2 units of array
             }
 
-            if (Input.GetKey(KeyCode.D))
+            if (Input.GetKey(KeyCode.D) && rightPageIndex < pagesMax)
             {
                 Debug.Log("next");
+                if (Input.GetKeyDown(KeyCode.D))
+                {
+                    rightPageIndex += 2;
+                    leftPageIndex += 2;
+                    pageInit(newPageLeft, pg1, leftPageIndex);
+                    pageInit(newPageRight, pg2, rightPageIndex);
+                }
                 //display next two units of array
             }
         }
+
+    }
+
+    void pageInit(GameObject pageType, GameObject pagePlacement, int pageIndex)
+    {
+        pageType.transform.position = new Vector3(pagePlacement.transform.position.x, pagePlacement.transform.position.y);
+        pageType.transform.SetParent(pagePlacement.transform);
+        pageType.GetComponent<SpriteRenderer>().sprite = pageSprite[pageIndex];
 
     }
 
--- Assets/MilesSceneStuff/Move.cs
+++ Assets/MilesSceneStuff/Move.cs
@@ -8,6 +8,8 @@ public class Move : MonoBehaviour
     public float speed;
     public Animator menuAnim;
 
+    public GameObject pageManager;
+
     // Update is called once per frame
     void Update()
     {
@@ -34,6 +36,18 @@ public class Move : MonoBehaviour
     void LRmove(float direction) //for movement left right
     {
         myBody.velocity = new Vector3(direction, myBody.velocity.y);
+    }
+
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "newPage")
+        {
+            pageManager.GetComponent<JournalManager>().pagesMax += 1;
+            pageManager.GetComponent<JournalManager>().pageSprite.Add(collision.gameObject.GetComponent<SpriteRenderer>().sprite);
+            Destroy(collision.gameObject);
+                
+        }
     }
 
 
